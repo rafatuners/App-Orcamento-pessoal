596c0a761d3c31be4e56085b9f180993c314f89b
Rafael Gagliardi Maciel da Cruz <111383387+rafatuners@users.noreply.github.com>
2023-08-02 17:49:29 -0300

    att

--- app.js
+++ app.js
@@ -40,7 +40,34 @@ class Bd{
         localStorage.setItem(id, JSON.stringify(d))
 
         localStorage.setItem('id', id)
-    }    
+    }   
+    
+    recuperarTodosRegistros() {
+
+        //array de despesas
+        let despesas = Array()
+
+        let id = localStorage.getItem('id')
+
+        //recuperar todas as despesas cadastradas
+        for(let i = 1; i <= id; i++) {
+
+            //recuperar a despesa
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+            //existe a possibilidade de haver índices que foram pulados // removidos
+            //nestes casos nós vamos pular esses índices.
+            if(despesa == null) {
+                continue
+            }
+
+
+            despesas.push(despesa)
+
+            
+        }
+        return despesas
+    }
 }
 
 let bd = new Bd()
@@ -86,3 +113,10 @@ function cadastrarDespesa() {
     
 }
 
+function carregaListaDespesas() {
+
+    let despesas = Array()
+    despesas = bd.recuperarTodosRegistros()
+
+    console.log(despesas)
+}
